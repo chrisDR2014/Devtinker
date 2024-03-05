97e237f76d72cc56ca1770014b8b5e2ce693565d
kricke87 <kricke87@gmail.com>
2024-03-04 22:27:06 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/boda/')
+def boda():
+    return render_template('boda.html')
+
+if __name__ == '__main__':
+    app.run()
+    #app.run(host="0.0.0.0", debug=True, port=8000)
