308abb1bb77360a01ee70036de51462d82180ded
chrisDR2014 <kricke87@gmail.com>
2024-03-16 22:41:08 -0400

    Fix links to pictures and menu

--- server.py
+++ server.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/destinations/')
+def destination():
+    return render_template('destination.html')
+
 @app.route('/boda/')
 def boda():
     return render_template('boda.html')
